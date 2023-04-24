4c6e4f85b0ac3207f935e0a11c3f636d00fa74e0
Roope Lehtinen <Roope98.rl@gmail.com>
2023-04-24 15:14:52 +0300

    Task 2-8 + begin of 2-9

--- osa2/puhelinluettelo-rl/src/App.js
+++ osa2/puhelinluettelo-rl/src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState } from "react"
 
 const Display = (props) => {
   const persons = props.persons
@@ -6,7 +6,7 @@ const Display = (props) => {
     <div>
       {persons.map((person) => (
         <div key = {person.name}>
-          <p>{person.name}</p>
+          <p>{person.name} {person.number}</p>
         </div>
       ))}
     </div>
@@ -14,15 +14,21 @@ const Display = (props) => {
 }
 
 const App = () => {
-  let [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
-  let [newName, setNewName] = useState('')
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-123456" },
+    { name: "Ada Lovelace", number: "39-44-5323523" },
+    { name: "Dan Abramov", number: "12-43-234345" },
+    { name: "Mary Poppendieck", number: "39-23-6423122" }
+  ])
+
+  const [newName, setNewName] = useState("")
+  const [newNumber, setNewNumber] = useState("")
 
   const addPerson = (event) => {
     event.preventDefault()
     const newPerson = {
-      name: newName
+      name: newName,
+      number: newNumber
     }
     const isCopy = persons.find(({name}) => name === newName)
     console.log(isCopy)
@@ -33,21 +39,39 @@ const App = () => {
       alert(`${newName} is already added to phonebook! Haha >:)`)
     }
     setNewName("")
+    setNewNumber("")
   }
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
   }
 
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+
   return (
     <div>
-      <h2>Phonebook</h2>
-      <form onSubmit={addPerson}>
-        <div>
-          name: <input value={newName} onChange={handleNameChange} />
-        </div>
-        <button type="submit">Add!</button>
-      </form>
+      <div>
+      <h1>Phonebook</h1>
+      </div>
+      <div>
+
+      </div>
+      <div>
+        <h2>Add a new person</h2>
+      </div>
+      <div>
+        <form onSubmit={addPerson}>
+          <div>
+            Name: <input type = "tel" value={newName} onChange={handleNameChange} />
+          </div>
+          <div>
+            Phonenumber: <input value={newNumber} onChange={handleNumberChange} />
+          </div>
+          <button type="submit">Add!</button>
+        </form>
+      </div>
       <h2>Numbers</h2>
       <Display persons = {persons} />
       <div>debug: {newName}</div>
