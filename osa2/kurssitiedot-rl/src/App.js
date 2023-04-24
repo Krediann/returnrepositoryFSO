94cb14dc661edfa3423010f5bb4c7c8863228c4e
Roope Lehtinen <Roope98.rl@gmail.com>
2023-04-24 13:13:24 +0300

    Task 2-4 and 2-5 + made the format of the code prettier

--- osa2/kurssitiedot-rl/src/App.js
+++ osa2/kurssitiedot-rl/src/App.js
@@ -1,73 +1,50 @@
-
-const Course =(props) => {
-  const course = props.course
-  return(
-    <div>
-      <Header course = {course} />
-      <Content course = {course} />
-    </div>
-  )
-}
-
-const Header = (props) => {
-  const course = props.course
-  return ( 
-    <div>
-      <h1>{course.name}</h1>
-    </div>
-  )
-}
-
-const Content = (props) => {
-  const course = props.course
-  return (
-    <div>
-      <Part course = {course} />
-      <Total course = {course} />
-    </div>
-  )
-}
-
-const Part = (props) => {
-  const course = props.course
-  return(
-    <div>
-      {course.parts.map(part =>(
-        <PartList key={part.id} part={part} />
-      ))}
-    </div>
-  )
-}
-
-const PartList = (props) => {
-  const part = props.part
-  return(
-    <div>
-      <p>{part.name} <b>{part.exercises}</b></p>
-    </div>
-  )
-}
-//Making this change just so I can commit Task 2-3 since I had already done it with reduce :D
-const Total = (props) => {
-  const course = props.course
-  const total = course.parts.reduce((total, part) => total + part.exercises, 0)
-  return(
-    <div>
-      <p>Total of <b>{total}</b> exercises in this course!</p>
-    </div>
-  )
-}
+import Course from "./components/Course";
 
 const App = () => {
-  const course = {
-    name: "Half Stack application development",
-    id: 1,
-    parts: [
-      {name: "Fundamentals of React", exercises: 10, id: 1},
-      {name: "Using props to pass data", exercises: 7, id: 2},
-      {name: "State of a component", exercises: 14, id: 3},
-      {name: "Kun tehtävä hajoaa", exercises: 2, id: 4}
-  ]}
+  const course = [
+    {
+      name: "Half Stack application development",
+      id: 1,
+      parts: [
+        {
+          name: "Fundamentals of React", 
+          exercises: 10, 
+          id: 1
+        },
+        {
+          name: "Using props to pass data", 
+          exercises: 7, 
+          id: 2
+        },
+        {
+          name: "State of a component", 
+          exercises: 14, 
+          id: 3
+        },
+        {
+          name: "Redux", 
+          exercises: 11, 
+          id: 4
+        }
+    ]},
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+          {
+            name: 'Routing',
+            exercises: 3,
+            id: 1
+          },
+          {
+            name: 'Middlewares',
+            exercises: 7,
+            id: 2
+          }
+      ]
+    }
+  ]
+
 
   return (
     <div>
---
+++ osa2/kurssitiedot-rl/src/components/Course.js
@@ -0,0 +1,64 @@
+const Course =(props) => {
+  const courses = props.course
+  return(
+    <div>
+      {courses.map((course) => (
+        <div key = {course.id}>
+          <Header course = {course} />
+          <Content course = {course} />
+        </div>
+        ))}
+    </div>
+  )
+}
+
+const Header = (props) => {
+  const course = props.course
+  return ( 
+    <div>
+      <h1>{course.name}</h1>
+    </div>
+  )
+}
+
+const Content = (props) => {
+  const course = props.course
+  return (
+    <div>
+      <Part course = {course} />
+      <Total course = {course} />
+    </div>
+  )
+}
+
+const Part = (props) => {
+  const course = props.course
+  return(
+    <div>
+      {course.parts.map(part =>(
+        <PartList key={part.id} part={part} />
+      ))}
+    </div>
+  )
+}
+
+const PartList = (props) => {
+  const part = props.part
+  return(
+    <div>
+      <p>{part.name} <b>{part.exercises}</b></p>
+    </div>
+  )
+}
+//Making this change just so I can commit Task 2-3 since I had already done it with reduce :D
+const Total = (props) => {
+  const course = props.course
+  const total = course.parts.reduce((total, part) => total + part.exercises, 0)
+  return(
+    <div>
+      <p>Total of <b>{total}</b> exercises in this course!</p>
+    </div>
+  )
+}
+
+export default Course;
